01c8394ee7740a16af02536b71941ad32ed1ea85
Gaurav Kapoor <gkapoordelhi@gmail.com>
2025-02-13 17:16:49 +0000

    updated code

--- zenoh_eval/src/zenoh_sub.cpp
+++ zenoh_eval/src/zenoh_sub.cpp
@@ -11,11 +11,13 @@
 #include "nav_msgs/msg/occupancy_grid.hpp"
 #include "nav2_msgs/msg/costmap.hpp"
 #include "map_msgs/msg/occupancy_grid_update.hpp"
+#include "std_msgs/msg/bool.hpp"
 
 #include <ctime>
 #include <sstream>
 
 using std::placeholders::_1;
+using namespace std::chrono_literals;
 
 class ZenohSubscriber : public rclcpp::Node
 {
@@ -30,7 +32,7 @@ class ZenohSubscriber : public rclcpp::Node
       std::tm* time_info = std::localtime(&file_time_int);
 
       std::stringstream time_stream;
-      time_stream << std::put_time(time_info, "%c");
+      time_stream << std::put_time(time_info, "%d-%m-%Y_%H-%M-%S");
 
       std::string time_str = time_stream.str();
 
@@ -68,83 +70,108 @@ class ZenohSubscriber : public rclcpp::Node
       costmap_sub_ = this->create_subscription<nav_msgs::msg::OccupancyGrid>("/summit/local_costmap/costmap", custom_qos, std::bind(&ZenohSubscriber::costmap_callback, this, _1));
       costmap_raw_sub_ = this->create_subscription<nav2_msgs::msg::Costmap>("/summit/local_costmap/costmap_raw", custom_qos, std::bind(&ZenohSubscriber::costmap_raw_callback, this, _1));
       costmap_updates_sub_ = this->create_subscription<map_msgs::msg::OccupancyGridUpdate>("/summit/local_costmap/costmap_updates", custom_qos, std::bind(&ZenohSubscriber::costmap_updates_callback, this, _1));
+
+      start_script_sub_ = this->create_subscription<std_msgs::msg::Bool>("/summit/start_zenoh_script", custom_qos, std::bind(&ZenohSubscriber::start_script_callback, this, _1)); 
+    }
+
+    void timer_callback()
+    {
+      RCLCPP_INFO(this->get_logger(), "5 mins finished");
+    }
+    
+    void start_script_callback(const std_msgs::msg::Bool::SharedPtr msg) 
+    {
+      start_script_ = msg->data;
+
+      if (start_script_ && flag_)
+      {   
+        timer_ = this->create_wall_timer(5min, std::bind(&ZenohSubscriber::timer_callback, this));
+        flag_ = false;
+      }
     }
 
     void image_raw_callback(const sensor_msgs::msg::Image::SharedPtr msg) 
     {
-      double msg_time = msg->header.stamp.sec + (1e-9 * msg->header.stamp.nanosec);
-      double time_now = this->get_clock()->now().seconds();
+      if (start_script_)
+      {   
+        double msg_time = msg->header.stamp.sec + (1e-9 * msg->header.stamp.nanosec);
+        double time_now = this->get_clock()->now().seconds();
 
-      image_raw_ << std::fixed << std::setprecision(3) << msg_time << " " << time_now << " " << msg->height << " " << msg->width << " " << msg->step << " " << msg->data.size() << std::endl;
-
-      // RCLCPP_INFO(this->get_logger(), "time in msg: %f secs", msg_time);
-      // RCLCPP_INFO(this->get_logger(), "time now: %f secs", time_now);
+        image_raw_ << std::fixed << std::setprecision(3) << msg_time << " " << time_now << " " << msg->height << " " << msg->width << " " << msg->step << " " << msg->data.size() << std::endl;
+        count_image_raw_++;
+      }
     }
 
     void image_rect_callback(const sensor_msgs::msg::Image::SharedPtr msg) 
     {
-      double msg_time = msg->header.stamp.sec + (1e-9 * msg->header.stamp.nanosec);
-      double time_now = this->get_clock()->now().seconds();
+      if (start_script_)
+      {  
+        double msg_time = msg->header.stamp.sec + (1e-9 * msg->header.stamp.nanosec);
+        double time_now = this->get_clock()->now().seconds();
 
-      image_rect_ << std::fixed << std::setprecision(3) << msg_time << " " << time_now << " " << msg->height << " " << msg->width << " " << msg->step << " " << msg->data.size() << std::endl;
-
-      // RCLCPP_INFO(this->get_logger(), "time in msg: %f secs", msg_time);
-      // RCLCPP_INFO(this->get_logger(), "time now: %f secs", time_now);
+        image_rect_ << std::fixed << std::setprecision(3) << msg_time << " " << time_now << " " << msg->height << " " << msg->width << " " << msg->step << " " << msg->data.size() << std::endl;
+        count_image_rect_++;
+      }
     }
 
     void image_raw_comp_callback(const sensor_msgs::msg::CompressedImage::SharedPtr msg) 
     {
-      double msg_time = msg->header.stamp.sec + (1e-9 * msg->header.stamp.nanosec);
-      double time_now = this->get_clock()->now().seconds();
+      if (start_script_)
+      { 
+        double msg_time = msg->header.stamp.sec + (1e-9 * msg->header.stamp.nanosec);
+        double time_now = this->get_clock()->now().seconds();
 
-      image_raw_comp_ << std::fixed << std::setprecision(3) << msg_time << " " << time_now << " " << msg->data.size() << std::endl;
-
-      // RCLCPP_INFO(this->get_logger(), "time in msg: %f secs", msg_time);
-      // RCLCPP_INFO(this->get_logger(), "time now: %f secs", time_now);
+        image_raw_comp_ << std::fixed << std::setprecision(3) << msg_time << " " << time_now << " " << msg->data.size() << std::endl;
+        count_image_raw_comp_++;
+      }
     }
 
     void image_stereo_callback(const sensor_msgs::msg::Image::SharedPtr msg) 
     {
-      double msg_time = msg->header.stamp.sec + (1e-9 * msg->header.stamp.nanosec);
-      double time_now = this->get_clock()->now().seconds();
+      if (start_script_)
+      { 
+        double msg_time = msg->header.stamp.sec + (1e-9 * msg->header.stamp.nanosec);
+        double time_now = this->get_clock()->now().seconds();
 
-      image_stereo_ << std::fixed << std::setprecision(3) << msg_time << " " << time_now << " " << msg->height << " " << msg->width << " " << msg->step << " " << msg->data.size() << std::endl;
-
-      // RCLCPP_INFO(this->get_logger(), "time in msg: %f secs", msg_time);
-      // RCLCPP_INFO(this->get_logger(), "time now: %f secs", time_now);
+        image_stereo_ << std::fixed << std::setprecision(3) << msg_time << " " << time_now << " " << msg->height << " " << msg->width << " " << msg->step << " " << msg->data.size() << std::endl;
+        count_image_stereo_++;
+      }
     }
 
     void costmap_callback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg) 
     {
-      double msg_time = msg->header.stamp.sec + (1e-9 * msg->header.stamp.nanosec);
-      double time_now = this->get_clock()->now().seconds();
+      if (start_script_)
+      { 
+        double msg_time = msg->header.stamp.sec + (1e-9 * msg->header.stamp.nanosec);
+        double time_now = this->get_clock()->now().seconds();
 
-      costmap_ << std::fixed << std::setprecision(3) << msg_time << " " << time_now << " " << msg->info.width << " " << msg->info.height << " " <<  msg->data.size() << std::endl;
-
-      // RCLCPP_INFO(this->get_logger(), "time in msg: %f secs", msg_time);
-      // RCLCPP_INFO(this->get_logger(), "time now: %f secs", time_now);
+        costmap_ << std::fixed << std::setprecision(3) << msg_time << " " << time_now << " " << msg->info.width << " " << msg->info.height << " " <<  msg->data.size() << std::endl;
+        count_costmap_++;
+      }
     }
 
     void costmap_raw_callback(const nav2_msgs::msg::Costmap::SharedPtr msg) 
     {
-      double msg_time = msg->header.stamp.sec + (1e-9 * msg->header.stamp.nanosec);
-      double time_now = this->get_clock()->now().seconds();
+      if (start_script_)
+      { 
+        double msg_time = msg->header.stamp.sec + (1e-9 * msg->header.stamp.nanosec);
+        double time_now = this->get_clock()->now().seconds();
 
-      costmap_raw_ << std::fixed << std::setprecision(3) << msg_time << " " << time_now << " " << msg->metadata.map_load_time.sec + (1e-9 * msg->metadata.map_load_time.nanosec) << " " << msg->metadata.update_time.sec + (1e-9 * msg->metadata.update_time.nanosec) << " "<< msg->metadata.size_x << " " << msg->metadata.size_y << " " << msg->data.size() << std::endl;
-
-      // RCLCPP_INFO(this->get_logger(), "time in msg: %f secs", msg_time);
-      // RCLCPP_INFO(this->get_logger(), "time now: %f secs", time_now);
+        costmap_raw_ << std::fixed << std::setprecision(3) << msg_time << " " << time_now << " " << msg->metadata.map_load_time.sec + (1e-9 * msg->metadata.map_load_time.nanosec) << " " << msg->metadata.update_time.sec + (1e-9 * msg->metadata.update_time.nanosec) << " "<< msg->metadata.size_x << " " << msg->metadata.size_y << " " << msg->data.size() << std::endl;
+        count_costmap_raw_++;
+      }
     }
 
     void costmap_updates_callback(const map_msgs::msg::OccupancyGridUpdate::SharedPtr msg) 
     {
-      double msg_time = msg->header.stamp.sec + (1e-9 * msg->header.stamp.nanosec);
-      double time_now = this->get_clock()->now().seconds();
+      if (start_script_)
+      { 
+        double msg_time = msg->header.stamp.sec + (1e-9 * msg->header.stamp.nanosec);
+        double time_now = this->get_clock()->now().seconds();
 
-      costmap_updates_ << std::fixed << std::setprecision(3) << msg_time << " " << time_now << " " << msg->width << " " << msg->height << " " << msg->data.size() << std::endl;
-
-      // RCLCPP_INFO(this->get_logger(), "time in msg: %f secs", msg_time);
-      // RCLCPP_INFO(this->get_logger(), "time now: %f secs", time_now);
+        costmap_updates_ << std::fixed << std::setprecision(3) << msg_time << " " << time_now << " " << msg->width << " " << msg->height << " " << msg->data.size() << std::endl;
+        count_costmap_updates_++;
+      }
     }
 
     ~ZenohSubscriber()
@@ -168,6 +195,8 @@ class ZenohSubscriber : public rclcpp::Node
     rclcpp::Subscription<nav2_msgs::msg::Costmap>::SharedPtr costmap_raw_sub_;
     rclcpp::Subscription<map_msgs::msg::OccupancyGridUpdate>::SharedPtr costmap_updates_sub_;
 
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr start_script_sub_;
+
     std::ofstream image_raw_;
     std::ofstream image_rect_;
     std::ofstream image_raw_comp_;
@@ -175,6 +204,20 @@ class ZenohSubscriber : public rclcpp::Node
     std::ofstream costmap_;
     std::ofstream costmap_raw_;
     std::ofstream costmap_updates_;
+
+    bool start_script_;
+
+    int count_image_raw_{0};
+    int count_image_rect_{0};
+    int count_image_raw_comp_{0};
+    int count_image_stereo_{0};
+    int count_costmap_{0};
+    int count_costmap_raw_{0};
+    int count_costmap_updates_{0};
+
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    bool flag_{true};
 };
 
 int main(int argc, char * argv[])
